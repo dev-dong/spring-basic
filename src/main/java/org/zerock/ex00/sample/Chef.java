885b3ccd8ec7bfbe6b40137dc0f5a2f07e08210e
dong <dho0647@naver.com>
2024-12-01 02:50:57 +0900

    스프링 설정

---
+++ src/main/java/org/zerock/ex00/sample/Chef.java
@@ -0,0 +1,12 @@
+package org.zerock.ex00.sample;
+
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.springframework.stereotype.Component;
+
+@Component
+@ToString
+@RequiredArgsConstructor
+public class Chef {
+
+}
